c4bca13cc3c4c378f7c6653585ca9e3c54146606
yaroslavsf <yaroslav1598753@gmail.com>
2023-10-14 12:05:00 +0200

    rename service users.service -> user.service

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -4,7 +4,7 @@ import { Repository } from 'typeorm';
 import {User} from './user.entity';
 
 @Injectable()
-export class UsersService {
+export class UserService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
