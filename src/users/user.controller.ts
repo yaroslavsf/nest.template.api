f048a5041ea6f87d29b9d51268394e9212f2d643
yaroslavsf <yaroslav1598753@gmail.com>
2023-10-26 13:05:19 +0200

    add sections in swagger

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,7 +1,9 @@
 import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
 import {User} from "./user.entity";
 import {UserService} from "./user.service";
+import {ApiTags} from "@nestjs/swagger";
 
+@ApiTags('users')
 @Controller('users')
 export class UserController {
     constructor(private userService: UserService) {}
